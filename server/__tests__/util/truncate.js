c620738dee7cf28fbce44a8769e7466686c1c3c9
Manoel Malafaia <mmalafaia@gmail.com>
2019-12-30 17:30:52 -0300

    Final test completed

--- server/__tests__/util/truncate.js
+++ server/__tests__/util/truncate.js
@@ -1,12 +1,18 @@
 import database from '../../src/database';
 
 export default function truncate() {
-  return Promise.all(
-    Object.keys(database.connection.models).map(key => {
-      return database.connection.models[key].detroy({
+  return Promise.all([
+    database.connection.query('SET FOREIGN_KEY_CHECKS = 0', null, {
+      raw: true,
+    }),
+    ...Object.keys(database.connection.models).map(key => {
+      return database.connection.models[key].destroy({
         truncate: true,
         force: true,
       });
-    })
-  );
+    }),
+    database.connection.query('SET FOREIGN_KEY_CHECKS = 1', null, {
+      raw: true,
+    }),
+  ]);
 }
