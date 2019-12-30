3d11290435418acb510d76f90f70819504b46880
Manoel Malafaia <mmalafaia@gmail.com>
2019-12-30 11:10:32 -0300

    first commit

---
+++ server/src/routes.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+
+const routes = new Router();
+
+routes.get('/', (req, res) => res.json({ message: 'Welcome to Omni CLI' }));
+
+/*
+routes.post('/users', (req, res) => {
+  return res.json({ id: 1 });
+});
+*/
+
+export default routes;
