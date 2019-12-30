8036cd994e21eadd687b9038518415edd02d2cb7
Manoel Malafaia <mmalafaia@gmail.com>
2019-12-30 17:30:40 -0300

    Test module completed

--- server/__tests__/integration/user.test.js
+++ server/__tests__/integration/user.test.js
@@ -1,7 +1,13 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +18,25 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Manoel Malafaia',
+        email: 'mmalafaia@gmail.com',
+        password_hash: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Manoel Malafaia',
+        email: 'mmalafaia@gmail.com',
+        password_hash: '123456',
+      });
+
+    expect(response.body).toBe(400);
   });
 });
